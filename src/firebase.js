322e3da417bcb237b480b62a922fc25d27a72b03
sairamdasari7 <106759140+sairamdasari7@users.noreply.github.com>
2024-12-12 10:59:00 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByVsI3_2hbx1cAoXwE-qv8vQAgC4IdnVo",
-  authDomain: "social-media-feed-app-cea49.firebaseapp.com",
-  projectId: "social-media-feed-app-cea49",
-  storageBucket: "social-media-feed-app-cea49.firebasestorage.app",
-  messagingSenderId: "810478302382",
-  appId: "1:810478302382:web:c169acffaf19c9069c293f",
-  measurementId: "G-R1XZFWM4TD"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
